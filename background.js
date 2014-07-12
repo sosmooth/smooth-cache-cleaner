88b46cfb91bd7c0bec2a86c8280f7bb06548ca9a
josuebrunel <josuebrunel@gmail.com>
2014-07-12 23:44:05 +0200

    bootstrap and jquery installed

--- background.js
+++ background.js
@@ -1,13 +1,31 @@
-chrome.browserAction.onClicked.addListener(function(tab){
+function cleanCache(tab){
+	$('document').ready(function(tab){
 	var tab_url = tab.url ;
 	/*
 		Check if current tab  url is among specified url
 		...
 	*/ 
-
+	alert('hello joe');
 	var millisecondPerWeek = 1000 * 60 * 60 * 24 * 7 ;
 	var oneWeekAgo = (new Date()).getTime() - millisecondPerWeek;
 
+	$('#submit').click(function(){
+		console.log('con japhet');
+		alert('con');
+	});
+
+	var val = $('#since').val();
+	console.log('con japhet');
+	alert('con');
 	// chrome.browsingData.removeCache({}, callback);
-})
+	})
+}
+
+chrome.browserAction.onClicked.addListener(function(tab){
+	alert('josh');
+	// $('#since').change(function(){
+	// 	var val = $('#since').val();
+	// 	alert(val);
+	// })
+});
 
