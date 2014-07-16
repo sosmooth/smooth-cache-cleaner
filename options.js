df6df8771182a09bcb38d1d373634678316d505a
josuebrunel <josuebrunel@gmail.com>
2014-07-16 17:39:42 +0200

    Getting options now he he

--- options.js
+++ options.js
@@ -1,13 +1,15 @@
 $('document').ready(function(){
+	var options = {};
 	// Catching click event
 	$('#submit').click(function(){
-		ck = $('.checkbox');
-		// console.log(ck)
+		
 		$('.checkbox input').each(function(){
 			var self = $(this);
-			if(self.is(':checked')){
-				console.log(self.attr('name') + ' is checked');
+			if(self.is(':checked')){ //If options checked
+				var name = self.attr('name');
+				options[name] = true;
 			}
 		})
+		console.log(options);
 	})
 })
