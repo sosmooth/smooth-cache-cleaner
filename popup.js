e4cdd6c6e9f0fec528768a88168489b132b1639b
josuebrunel <josuebrunel@gmail.com>
2015-01-23 21:19:38 +0100

    testing stuff

--- popup.js
+++ popup.js
@@ -21,19 +21,6 @@ $('document').ready(function(){
 	// Cache la div dès que le DOM est prêt
 	$('#container_options').hide();
 
-	// Affiche la div au clique sur l'engrenage (home)											
-	// $('#gearing-options').click(function() {
-	// 	$('#container').hide();
-	// 	$('#container_options').show();
-	// 	return false;
-	// });
-
-	// $('#gearing-home').click(function() {
-	// 	$('#container_options').hide();
-	// 	$('#container').show();
-	// 	return false;
-	// });
-
 	// Effet de déplacement de la div
 	$('#gearing-options').click(function() {
 		$('#container_options').toggle(400);
